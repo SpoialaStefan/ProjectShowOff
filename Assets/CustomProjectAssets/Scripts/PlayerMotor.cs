211591f9f816652b400de2f7e786681cd610f322
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-04-29 15:03:25 +0200

    Player motor start

--- Assets/CustomProjectAssets/Scripts/PlayerMotor.cs
+++ Assets/CustomProjectAssets/Scripts/PlayerMotor.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
+    [Tooltip("Mouse Sensitivity for rotation")]
+    [SerializeField]
+    private float mouseSensitivity = 100;
+    [Tooltip("Lift off force. Must have positive value")]
+    [SerializeField]
+    private float UpForce = 50f;
+    [SerializeField]
+    private float ForwardSpeed = 20f;
+    [SerializeField]
+    private float SideMovement = 15f;
+    [SerializeField]
+    private Transform model;
+
+
+    private Rigidbody rigidbody;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        MoveUpDown();
+        MoveForward();
+        Rotation();
+        SwerweLeftRight();
+    }
+
+    private void MoveUpDown()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * UpForce);
+        }
+    }
+
+    private void MoveForward()
+    {
+        if (Input.GetAxis("Vertical")!=0)
+        {
+            rigidbody.AddRelativeForce(Vector3.forward * ForwardSpeed * Input.GetAxis("Vertical"));
+        }
+    }
+
+    private void Rotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") *mouseSensitivity* Time.deltaTime;
+        transform.Rotate(Vector3.up*mouseX);
+    }
+
+    float tiltAmountSidewayes;
+    float tiltAmountVelocity;
+    private void SwerweLeftRight()
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            rigidbody.AddRelativeForce(Vector3.right * Input.GetAxis("Horizontal") * SideMovement);
+            tiltAmountSidewayes = Mathf.SmoothDamp(tiltAmountSidewayes, -20 * Input.GetAxis("Horizontal"), ref tiltAmountVelocity, 0.1f);
+        }
+        else
+        {
+            tiltAmountSidewayes = Mathf.SmoothDamp(tiltAmountSidewayes, 0, ref tiltAmountVelocity, 0.1f);
+        }
+        model.rotation = Quaternion.Euler(new Vector3(model.rotation.x, model.rotation.y, tiltAmountSidewayes));
     }
 }
