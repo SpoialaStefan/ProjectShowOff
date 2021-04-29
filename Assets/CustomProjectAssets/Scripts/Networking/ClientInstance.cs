0df00b6eecf02fd7efee39e700d0ec79aef6dd65
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-04-29 11:46:51 +0200

    Multiplayer Setup Start
    Setup player base prefab and network manager

---
+++ Assets/CustomProjectAssets/Scripts/Networking/ClientInstance.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ClientInstance : NetworkBehaviour
+{
+
+    public static ClientInstance Instance;
+
+    [SerializeField]
+    private NetworkIdentity _playerPrefab = null;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        NetworkSpawnPlayer();
+    }
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        Instance = this;
+    }
+
+    [Server]
+    private void NetworkSpawnPlayer()
+    {
+        GameObject playerClient = Instantiate(_playerPrefab.gameObject, transform.position, Quaternion.identity);
+        NetworkServer.Spawn(playerClient, base.connectionToClient);
+    }
+
+    public static ClientInstance GetClientInstance(NetworkConnection conn = null)
+    {
+        if (NetworkServer.active && conn != null)
+        {
+            NetworkIdentity localPlayer;
+            if (CustomNetworkManager.LocalPlayers.TryGetValue(conn, out localPlayer))
+                return localPlayer.GetComponent<ClientInstance>();
+            else
+                return null;
+        }
+        else
+        {
+            return Instance;
+        }
+    }
+}
