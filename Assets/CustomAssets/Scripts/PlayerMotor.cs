bfa254d0a697af9244a8cc4fd2d793a872b5ee9e
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-05-11 14:50:15 +0200

    Third person controller

--- Assets/CustomAssets/Scripts/PlayerMotor.cs
+++ Assets/CustomAssets/Scripts/PlayerMotor.cs
@@ -9,12 +9,16 @@ public class PlayerMotor : MonoBehaviour
     [SerializeField]
     private float ForwardSpeed = 6f;
     [SerializeField]
-    private float turnSmoothTime = 0.1f; 
-    
+    private float UpSpeed = 3f;
+    [SerializeField]
+    private float DownSpeed = 1f;
+    [SerializeField]
+    private float turnSmoothTime = 0.1f;
+
     private float turnSmoothVelocity = 0.1f;
 
     private CharacterController controller;
-
+    private float directionY = 0f;
     // Start is called before the first frame update
     void Awake()
     {
@@ -33,16 +37,33 @@ public class PlayerMotor : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //directionY += UpSpeed;
+            controller.Move(Vector3.up * UpSpeed * Time.deltaTime);
+        }
+
+        if (!controller.isGrounded)
+        {
+            //directionY -= DownSpeed;
+            controller.Move(Vector3.up * DownSpeed * -1 * Time.deltaTime);
+        }
+
+
+        //direction.y = directionY;
+
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg+camera.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0, angle, 0);
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f)*Vector3.forward;
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * ForwardSpeed * Time.deltaTime);
         }
     }
 
-   
+
 }
